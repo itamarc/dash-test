5c3bc83b1499daa129c2dd41c097a108e3fd9f35
Itamar Carvalho <itamarc@gmail.com>
2021-09-03 18:36:07 -0300

    Started callback implementation

--- webui/apps/job_loc.py
+++ webui/apps/job_loc.py
@@ -1,20 +1,12 @@
+from app import app
 from datetime import date
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
 import data_source as ds
-
-data = ds.get_jobs_by_location()
+from dash.dependencies import Input, Output
 
 fig = go.Figure(go.Scattergeo())
-fig.update_geos(
-    visible=False, resolution=110, scope="europe",
-    showcountries=True, countrycolor="Black",
-    showsubunits=True, subunitcolor="Blue",
-    showland=True, landcolor="khaki",
-    showocean=True, oceancolor="LightBlue"
-)
-fig.update_layout(height=500, margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
 layout = html.Div([
     html.Div([
@@ -35,7 +27,9 @@ layout = html.Div([
             dcc.DatePickerRange(
                 id='my-date-picker-range',
                 min_date_allowed=date(1995, 8, 5),
-                max_date_allowed=date(2017, 9, 19),
+                max_date_allowed=date(2021, 9, 19),
+                start_date=date(2021, 8, 3),
+                end_date=date(2021, 9, 3),
                 initial_visible_month=date(2021, 8, 1),
             )], className='date_range_select'),
     ], className='options_container'),
@@ -44,3 +38,21 @@ layout = html.Div([
         figure=fig
     )
 ])
+
+
+@app.callback(
+    Output('jobs-by-location', 'figure'),
+    Input('job-title-dropdown', 'value'),
+    Input('my-date-picker-range', 'start_date'),
+    Input('my-date-picker-range', 'end_date'))
+def update_graph(job_title, start_date, end_date):
+    data = ds.get_jobs_by_location(job_title, start_date, end_date)
+    fig.update_geos(data,
+                    visible=False, resolution=110, scope="europe",
+                    showcountries=True, countrycolor="Black",
+                    showsubunits=True, subunitcolor="Blue",
+                    showland=True, landcolor="LightGrey",
+                    showocean=True, oceancolor="LightBlue"
+                    )
+    fig.update_layout(height=500, margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    return fig
--- webui/data_source.py
+++ webui/data_source.py
@@ -1,5 +1,28 @@
-def get_jobs_by_location() -> list:
+import pandas as pd
+
+
+def get_jobs_by_location(job_title, start_date, end_date) -> pd.DataFrame:
     """
     Return a list of jobs in the database that are in the location specified
     """
-    return []
+    data = [
+        {
+            'country': 'nl',
+            'job_offers': 321
+        },
+        {
+            'country': 'uk',
+            'job_offers': 123
+        },
+        {
+            'country': 'pt',
+            'job_offers': 12
+        },
+        {
+            'country': 'it',
+            'job_offers': 23
+        },
+    ]
+    # The data above did not work as is
+    jobs_df = pd.DataFrame([])
+    return jobs_df
